fbaee4af8ea2bac826f3808597894b8b515b6ebd
Pushpak Gosavi <pushpak.gosavi7@gmail.com>
2025-02-09 00:13:39 +0530

    master:Stable, Implemented the SwapList problem

---
+++ src/main/kotlin/SwapSpecificList.kt
@@ -0,0 +1,16 @@
+fun main(){
+    val list= listOf(1,2,3,4,5,6,7) // output (5,6,7,1,2,3,4)
+    println(swapList(list, 3))
+
+}
+
+fun swapList(list:List<Int>, swapNumber: Int):MutableList<Int>{
+    val swapList = mutableListOf<Int>()
+    for (i in list.size-swapNumber until list.size){
+        swapList.add(list[i])
+    }
+    for (j in 0 until  list.size-swapNumber){
+        swapList.add(list[j])
+    }
+    return swapList
+}
